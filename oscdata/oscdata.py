e718a813f40a7dcda9f9d20a85539376f97be2ff
Mi Dai <nita918@gmail.com>
2019-03-29 14:57:37 -0400

    add oscdata class

--- oscdata/oscdata.py
+++ oscdata/oscdata.py
@@ -1,1 +1,18 @@
 #osc data to pandas df
+
+class OSCData():
+    def __init__(self):
+        pass
+
+
+    def get_metadata(self):
+        pass
+
+
+    def get_photometry(self):
+        pass
+
+
+
+    def get_spectra(self):
+        pass
