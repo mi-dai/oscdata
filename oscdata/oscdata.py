b64e5829255e10c9ab107eafc2183f8901ee5494
kdettman <dettman@physics.rutgers.edu>
2019-03-29 15:33:39 -0400

    revert

--- oscdata/oscdata.py
+++ oscdata/oscdata.py
@@ -24,6 +24,7 @@ class OSCData():
         self.photometry['MJD'] = photometry['time']
         self.photometry['Survey'] = photometry['telescope']
         self.photometry['Name'] = name
+        #comment
 
 
 
