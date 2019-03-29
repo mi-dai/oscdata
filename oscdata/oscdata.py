735adc2e252008bb7850e89866ffa76a116e1db4
kdettman <dettman@physics.rutgers.edu>
2019-03-29 15:02:47 -0400

    Added initiator attributes

--- oscdata/oscdata.py
+++ oscdata/oscdata.py
@@ -1,8 +1,10 @@
 #osc data to pandas df
 
 class OSCData():
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+    	self.data = pd.read_json(filename)
+    	self.name = filename.split(['.json'])
+        
 
 
     def get_metadata(self):
@@ -10,6 +12,17 @@ class OSCData():
 
 
     def get_photometry(self):
+
+    	photometry = pd.read_json(pd.DataFrame(data['photometry']).to_json(),orient='column')
+		print(photometry.columns)
+		photometry.rename(columns={x: x.encode('ascii') for x in photometry.columns})
+		photometry = Table.from_pandas(photometry)    
+		photometry['Filter'] = photometry['band']
+		photometry['Mag'] = photometry['magnitude']
+		photometry['MagErr'] = photometry['e_magnitude']
+		photometry['MJD'] = photometry['time']
+		photometry['Survey'] = photometry['telescope']
+
         pass
 
 
