d8636f925417252091917af5b4caf92f356c9a9d
Mi Dai <nita918@gmail.com>
2019-03-29 15:26:47 -0400

    commit

--- oscdata/oscdata.py
+++ oscdata/oscdata.py
@@ -1,31 +1,32 @@
 #osc data to pandas df
+import pandas as pd
+import numpy as np
 
 class OSCData():
     def __init__(self, filename):
-    	self.data = pd.read_json(filename)
-    	self.name = filename.split(['.json'])
+        data = pd.read_json(filename)
+        self.name = filename.split('.json')
+        self.data = data[self.name]
         
 
 
     def get_metadata(self):
         pass
+        
 
 
     def get_photometry(self):
 
-    	photometry = pd.read_json(pd.DataFrame(data['photometry']).to_json(),orient='column')
-		print(photometry.columns)
-		photometry.rename(columns={x: x.encode('ascii') for x in photometry.columns})
-		photometry = Table.from_pandas(photometry)    
-		photometry['Filter'] = photometry['band']
-		photometry['Mag'] = photometry['magnitude']
-		photometry['MagErr'] = photometry['e_magnitude']
-		photometry['MJD'] = photometry['time']
-		photometry['Survey'] = photometry['telescope']
-
-        pass
-
-
+        photometry = pd.read_json(pd.DataFrame(self.data['photometry']).to_json(),orient='column')
+        print(photometry.columns)
+        photometry.rename(columns={x: x.encode('ascii') for x in photometry.columns})
+        photometry = Table.from_pandas(photometry)    
+        photometry['Filter'] = photometry['band']
+        photometry['Mag'] = photometry['magnitude']
+        photometry['MagErr'] = photometry['e_magnitude']
+        photometry['MJD'] = photometry['time']
+        photometry['Survey'] = photometry['telescope']
+        print(photometry)
 
     def get_spectra(self):
         pass
